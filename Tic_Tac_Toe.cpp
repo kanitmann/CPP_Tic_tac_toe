dd07130cffa1842ce71169c21e929c9197c4a5d4
Kanit <kanitmann90@gmail.com>
2021-05-15 21:21:18 +0530

    Code Completed :sparkles: :rocket:

--- Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe.cpp
@@ -5,10 +5,18 @@ using namespace std;
 int board[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 int state[][3] = {{9, 9, 9}, {9, 9, 9}, {9, 9, 9}};
 
-int win_checker(int board[][3], int m)
+int win_checker(int state[][3], int m)
 {
-    cout << "\n \t Winchecker working"<<endl;
-    return 0;
+    if((state[0][0]==1 && state[1][1]==1 && state[2][2]==1)||(state[0][0]==1 && state[0][1]==1 && state[0][2]==1)||(state[0][0]==1 && state[1][0]==1 && state[2][0]==1)||(state[1][0]==1 && state[1][1]==1 && state[1][2]==1)||(state[2][0]==1 && state[2][1]==1 && state[2][2]==1)||(state[1][0]==1 && state[1][1]==1 && state[1][2]==1)||(state[2][0]==1 && state[2][1]==1 && state[2][2]==1)||(state[0][2]==1 && state[1][1]==1 && state[2][1]==1)||(state[0][0]==0 && state[1][1]==0 && state[2][2]==0)||(state[0][0]==0 && state[1][0]==0 && state[2][0]==0)||(state[1][0]==1 && state[1][1]==0 && state[1][2]==0)||(state[2][0]==0 && state[2][1]==0 && state[2][2]==0)||(state[0][0]==0 && state[0][1]==0 && state[0][2]==0)||(state[1][0]==0 && state[1][1]==0 && state[1][2]==0)||(state[2][0]==0 && state[2][1]==0 && state[2][2]==0)||(state[0][2]==0 && state[1][1]==0 && state[2][1]==0))
+    {
+        cout << "\n\n\tWinner found\n\n";
+        return 1;
+    }
+    else
+    {
+        cout<<"\nRunning Win_Checker!!\n";
+        return 0;
+    }
 }
 
 int printer(int board[][3], int m)
@@ -32,7 +40,7 @@ int placement(int board[][3], int m, int i)
 {
     int place;
     cout << i << "'s turn" << endl;
-
+    int winner = 0;
 XAction:
     cout << "\nWhere would you like to place '" << i << "'?";
     cin >> place;
@@ -41,7 +49,6 @@ XAction:
         cout << "\nAlready filled or point not valid, please retry.";
         goto XAction;
     }
-
     for (int j = 1; j <= 9; j++)
     {
         if (j == place)
@@ -52,8 +59,11 @@ XAction:
         }
     }
 
-    win_checker(::board, 3);
-    return 0;
+    winner = win_checker(state, 3);
+    if(winner==1)
+        return 1;
+    else
+        return 0;
 }
 
 int main()
@@ -62,9 +72,12 @@ int main()
     int place = 0;
     cout << "Welcome to Tic Tac Toe";
     printer(board, m);
+    int win = 0;
     for (int i = 0; i < 9; i++)
     {
-        placement(board, m, i % 2);
+        win = placement(board, m, i % 2);
+        if(win == 1)
+            break;
     }
 
     cout << "Printing Final:\n ";
