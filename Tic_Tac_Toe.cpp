120e23e8deafa9047cabf2a124aaa84f10b27d17
Kanit <kanitmann90@gmail.com>
2021-05-15 13:09:03 +0530

    Added place functions :rockets:

--- Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe.cpp
@@ -24,12 +24,28 @@ char printer(char board[][3], int m)
     return '0';
 }
 
+int placeO(char board[][3], int m, int i)
+{
+
+}
+
+int placeX(char board[][3], int m, int i)
+{
+
+}
+
 int main()
 {
     int m = 3;
+    int place = 0;
     char board[][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
     int state[][3] = {{'0','0','0'},{'0','0','0'},{'0','0','0'}};
     cout<<"Welcome to Tic Tac Toe \n\n 'O' goes first, where would you like to place your 'O'?";
     printer(board, m);
+    for(int i = 0 ; i < 9 ; i++)
+    {
+        cin >> place;
+        i%2==0 ? placeO(board ,m , i) : placeX(board ,m, i); 
+    }
     return 0;
 }
